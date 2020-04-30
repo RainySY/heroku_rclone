d70ab8f90c71d87b608e92b2c0a70ca84db5699e
Xiaowei Huang <x68huang@uwaterloo.ca>
2020-04-30 19:06:53 +0800

    add deploy script and README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # heroku_rclone
 A rclone docker image
+
+# Deploy instructions
+
+1. 把SA放进/root/sa文件夹中
+2. 修改/root/rclone.conf 
+3. 运行bash delpoy.sh [COMMAND]
+
+# Example
+
+```
+bash deploy.sh rclone copy --progress -v gd1:download gd2:download
+```
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+heroku login -i
+heroku container:login
+
+heroku create --remote app1
+heroku container:push web --remote app1
+heroku container:release web --remote app1
+heroku ps:stop web --remote app1
+heroku run:detached "@" --remote app1
