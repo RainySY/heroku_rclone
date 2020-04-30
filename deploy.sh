71dc70dff4313e98742b8891d5624943ae74ec63
Xiaowei Huang <x68huang@uwaterloo.ca>
2020-04-30 19:15:51 +0800

    bugfix

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ heroku create --remote app1
 heroku container:push web --remote app1
 heroku container:release web --remote app1
 heroku ps:stop web --remote app1
-heroku run:detached "@" --remote app1
+heroku run:detached "$@" --remote app1
--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -7,7 +7,7 @@ mkdir -p /opt/rclone
 export PATH=/opt/rclone:$PATH
 export RCLONE_CONFIG=/rclone.conf
 wget -O /tmp/rclone.zip https://github.com/xhghs/files/releases/download/0.1.1/rclone_amd64.zip
-unzip /tmp/rclone.zip -d /opt/rclone
+unzip -f /tmp/rclone.zip -d /opt/rclone
 chmod +x /opt/rclone/rclone
 
 exec "$@"
